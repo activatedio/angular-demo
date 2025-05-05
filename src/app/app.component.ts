7694d1db9a9056f29deb39513cd6d7983d2646eb
kobrien@activated.io <kienan.obrien@authwise.com>
2025-05-05 15:57:56 -0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
+import {
+  MatSidenavModule
+} from "@angular/material/sidenav";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatTabsModule, MatButtonModule, MatNavList, MatListItem],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
